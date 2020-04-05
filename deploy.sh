0e4bab16eb8b29fc71d243f0e273cfcf36bc4458
Miguel Beltrán Sanz <miguelbeltransanz@gmail.com>
2020-04-05 20:51:53 +0200

    make the build process in another branch

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,34 @@ branch_name=${branch_name##refs/heads/}
 
 if [ $branch_name = "master" ]; then
   echo 'Deploying...'
+  CHANGED=$(git diff-index --name-only HEAD --)
+
+  git status
+
+  if [ -n "$CHANGED" ]; then
+    echo "[ERROR] You cannot deploy with pending changes"
+    exit 1
+  fi
+
   echo 'Building site'
   docker-compose run app jekyll build
+
+  echo 'Checking for local broken links'
   docker-compose run app htmlproofer --checks-to-ignore=ImageCheck --internal-domains localhost --disable-external ./_site
   if [ $? -ne 0 ] ; then echo "[ERROR] Broken link" ; exit 1; fi
-  git add .
-  git commit -m 'Just another deploy'
+
   echo 'Synching repository'
   git push origin master -f
+
+  echo 'Switching to build branch'
+  git checkout build
+  git add .
+  git commit -m 'Just another deploy'
   git push eyl master -f
+
+  echo 'Switching to master branch'
+  git checkout master
+
   echo 'Done'
 else
   echo 'Deploy script must be run in master'
